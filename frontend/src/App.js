91ba0379c77b31f2e8eb430bea536155eceaeaca
Gpooja13 <145600866+Gpooja13@users.noreply.github.com>
2024-01-13 18:25:13 +0530

    modified path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,30 +14,55 @@ import React, { createContext, useState } from "react";
 import { LoginContext } from "./context/LoginContext";
 import UserProfile from "./screens/UserProfile";
 import Explore from "./screens/Explore";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
   const [userLogin, setUserLogin] = useState(false);
   return (
     <BrowserRouter>
-    <GoogleOAuthProvider clientId="715281373099-rnp1d2vte3fr364qsui8fou90lu598t5.apps.googleusercontent.com">
-      <LoginContext.Provider value={{ setUserLogin, setModalOpen }}>
-        <Navbar login={userLogin} modalOpen={modalOpen} />
-        <Routes>
-          <Route path="long-lime-penguin-wear.cyclic.app/" element={<Home />} />
-          <Route path="long-lime-penguin-wear.cyclic.app/signIn" element={<SignIn />} />
-          <Route path="long-lime-penguin-wear.cyclic.app/signUp" element={<SignUp />} />
-          <Route path="long-lime-penguin-wear.cyclic.app/profile" element={<Profile />} />
-          <Route path="long-lime-penguin-wear.cyclic.app/createPost" element={<CreatePost />} />
-          <Route path="long-lime-penguin-wear.cyclic.app/profile/:userid" element={<UserProfile />} />
-          <Route path="long-lime-penguin-wear.cyclic.app/explore" element={<Explore />} />
-          <Route path="long-lime-penguin-wear.cyclic.app/*" element={<Error />} />
-        </Routes>
-        <ToastContainer />
-        {modalOpen && <Modal/>}
-      </LoginContext.Provider>
-      </GoogleOAuthProvider>;
+      <GoogleOAuthProvider clientId="715281373099-rnp1d2vte3fr364qsui8fou90lu598t5.apps.googleusercontent.com">
+        <LoginContext.Provider value={{ setUserLogin, setModalOpen }}>
+          <Navbar login={userLogin} modalOpen={modalOpen} />
+          <Routes>
+            <Route
+              path="long-lime-penguin-wear.cyclic.app/"
+              element={<Home />}
+            />
+            <Route
+              path="long-lime-penguin-wear.cyclic.app/signIn"
+              element={<SignIn />}
+            />
+            <Route
+              path="long-lime-penguin-wear.cyclic.app/signUp"
+              element={<SignUp />}
+            />
+            <Route
+              path="long-lime-penguin-wear.cyclic.app/profile"
+              element={<Profile />}
+            />
+            <Route
+              path="long-lime-penguin-wear.cyclic.app/createPost"
+              element={<CreatePost />}
+            />
+            <Route
+              path="long-lime-penguin-wear.cyclic.app/profile/:userid"
+              element={<UserProfile />}
+            />
+            <Route
+              path="long-lime-penguin-wear.cyclic.app/explore"
+              element={<Explore />}
+            />
+            <Route
+              path="long-lime-penguin-wear.cyclic.app/*"
+              element={<Error />}
+            />
+          </Routes>
+          <ToastContainer />
+          {modalOpen && <Modal />}
+        </LoginContext.Provider>
+      </GoogleOAuthProvider>
+      ;
     </BrowserRouter>
   );
 }
